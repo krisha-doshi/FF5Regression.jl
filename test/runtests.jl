2d8eced9d2cc5d9639841a7bfd2e5e5196a2b295
krisha <krisha.doshi1@gmail.com>
2023-05-30 15:24:07 +0800

    Moving test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using .FF5Regression
 using Test
 using GLM, DataFrames, CSV, Plots, Lathe, StatsPlots, MLDataUtils, MLBase, HTTP, ZipFile, Dates
 
-FF5Regression.regression_data("aapl", "1999-12-12", "2022-12-12")
 
 @testset "FF5Regression.jl" begin
+    FF5Regression.regression_data("aapl", "1999-12-12", "2022-12-12")
     # Write your tests here.
 end
